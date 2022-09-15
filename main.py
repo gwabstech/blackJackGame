824ebacab2fb0794225d2e1abda88504064cda9b
Abubakar <Gwabstech@gmail.com>
2022-09-15 10:33:59 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,32 @@
+import random
+
+
+# this function return a random number generated from the card list
+def dealCard():
+    cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+    random_card = random.choice(cards)
+    return random_card
+
+
+user_card = []
+comuter_cards = []
+for i in range(2):
+    user_card.append(dealCard)
+    comuter_cards.append(dealCard)
+
+# this function calculate and return the sum of a list "cards"
+
+
+def calculate_score(cards):
+
+    if sum(cards) == 21 and len(cards) == 2:
+        return 0
+    if 11 in cards and sum(cards) > 21:
+        cards.remove(11)
+        cards.append(1)
+
+    return sum(cards)
+
+
+user_score = calculate_score(user_card)
+computer_score = calculate_score(comuter_cards)
